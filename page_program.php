35e178a682877c6522fc51e0b34dbd9f18c7d082
Ega Bagus Purnama <egabagus1@gmail.com>
2025-02-10 21:12:29 +0700

    Fix custom style

--- page_program.php
+++ page_program.php
@@ -23,21 +23,6 @@
 			</div>
 		<?php endwhile; ?>
 	<?php endif;
-	wp_reset_postdata();
-
-	if ($parent->have_posts()) : ?>
-		<?php while ($parent->have_posts()) : $parent->the_post(); ?>
-			<div id="parent-<?php the_ID(); ?>" class="page-program-item row">
-				<div class="col-lg-3">
-					<?php the_post_thumbnail(array(156, false), array('class' => 'img-responsive program-thumb', 'title' => 'Feature image')); ?>
-				</div>
-				<div class="col-lg-9">
-					<h3 class="heading-3"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
-					<?php the_content(); ?>
-				</div>
-			</div>
-		<?php endwhile; ?>
-	<?php endif;
 	wp_reset_postdata(); ?>
 </div>
 <?php get_footer();
